3797895dc9e26208d8630858ec302ce985d22684
veeramacahneni <rm466724@dal.ca>
2019-05-23 05:20:26 -0700

    task0.link

---
+++ task0.py
@@ -0,0 +1,25 @@
+protected void btn1_Click(object sender, EventArgs e)
+    {
+        for (int i = 1; i < 101; i++)
+        {
+            if (i % 3 == 0 & i % 5 == 0)
+            {
+                Response.Write("fizzbuzz" + ",");
+            }
+            else if (i % 3 == 0)
+            {
+                Response.Write("fizz" + ",");
+            }
+            else if (i % 5 == 0)
+            {
+                Response.Write("buzz" + ",");
+            }
+            else
+            {
+                i = i + 0;
+            }
+
+            Response.Write(i +",");
+        }
+
+    }
